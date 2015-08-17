1059dd2dfac4e5b60944426da97f671918f81a5f
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2015-08-17 15:50:59 -0700

    updated iterator.js

--- week-02/day-01-arrays+Iterators/dusk-iteratorsLab/starterCode/iterators.js
+++ week-02/day-01-arrays+Iterators/dusk-iteratorsLab/starterCode/iterators.js
@@ -2,11 +2,22 @@
 var o_o = {
   each: function(list, callback) {
     //code here...
+
   },
   map: function(list, callback){
     //code here...
   },
   filter: function(list, callback) {
+
+  }
+};
+
+//expose our iterators to other files
+//such as our assertions
+module.exports = o_o;
+
+/**
+
     var output = [];
       for (var i = 0; i < list.length; i++) {
         if (callback(list[i])) {
@@ -14,9 +25,4 @@ var o_o = {
         }
       }
       return output;
-  }
-};
-
-//expose our iterators to other files
-//such as our assertions
-module.exports = o_o;
+*/
