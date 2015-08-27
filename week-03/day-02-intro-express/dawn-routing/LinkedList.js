057a8b6dbace8722b07d0e5727f063c04da72e04
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2015-08-27 11:02:25 -0700

    Update with week 3, day 4 code

--- week-03/day-02-intro-express/dawn-routing/LinkedList.js
+++ week-03/day-02-intro-express/dawn-routing/LinkedList.js
@@ -95,6 +95,18 @@ List.prototype = {
 
     },
 
+            //Alternate, multiine method:
+    // length: function() {
+    //     var current = this.start;
+    //     var i = 0;
+    //     while(current !== null) {
+    //         i++;
+    //         current = current.next;
+    //     }
+    //     return i;
+    // },
+
+
     /*
         Method: exists
         Traverse the list. If a Node with the data passed in exists, then return
@@ -153,7 +165,17 @@ List.prototype = {
     indexOf: function(data) {
         // Enter code here!
 
-        var current = this.start;
+        var node = this.start;
+        var i = 0;
+    
+        while (node !== null) {
+            if (node.data === data) {
+                return i;
+            } else {
+                node = node.next;
+            }
+            i = i + 1;
+        }
 
 
     },
@@ -164,6 +186,21 @@ List.prototype = {
               */
     dataFrom: function(i) {
         // Enter code here!
+             var current = this.start; 
+
+             var count = 0;
+             while (current !== null) {
+                if (count == i) {
+                    return current.data;
+                count++;
+                current = current.next
+                }
+            };
+
+
+            //if will return the data 
+            // else will go to next node in LinkedList
+
     },
 
     /*
@@ -171,8 +208,30 @@ List.prototype = {
         Traverse the List.  Find the ith Node in the list and insert a new Node
          after it.  You must preserve the list structure!
     */
-    insertAt: function(i, data){
-        // Enter code here!
+     insertAt: function(i, data){
+        var previousNode;
+        var newNode = this.makeNode();
+        newNode.data = data;
+        var current = this.start;
+        var index = 0;
+        if(i > this.length() + 1){
+            return "index out of range";
+        }
+        if(i === 0){
+            this.insertAtHead(data);
+        }
+        while(current !== null){
+            if(index === i - 1){
+                newNode.next = current.next;
+                current.next = newNode;
+                if(i === this.length() - 1){
+                    this.end = newNode;
+                }
+                return "inserted";
+            }
+            index++;
+            current = current.next;
+        }
     },
 
     /*
@@ -183,8 +242,22 @@ List.prototype = {
     */
     delete: function(data) {
         // Enter code here!
+        var current = this.start;
+        var i = 0;
+
+        while(current !== null){
+            if (current.data === data) {
+                
+            }
+
+        }
+
     }
 }
+
+//find the node with the corresponding data
+//remove the node
+//Add the subsequent node in its place
 
 
 /* LinkedList initialization */
@@ -203,17 +276,18 @@ LinkedList.print();
 /* Run your functions here to test. */
 //Insert functions here to to test.
 
+console.log(LinkedList.insertAt(3, "burger"));
 
 
 
 /** .each function - check each method
-function accessor(node) {
-    console.log(node.data + " has been accessed.");
-}
-
 LinkedList.each( function(node){
     console.log(node.data + " has been accessed.");
 })
+
+function accessor(node) {
+    console.log(node.data + " has been accessed.");
+}
 
 LinkedList.each(accessor);
 */
