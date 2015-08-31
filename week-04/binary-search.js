107aa00a651c32010cb48647077c33a38d0699be
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2015-08-31 10:44:28 -0700

    committing to pull week-04 day 1 updates

---
+++ week-04/binary-search.js
@@ -0,0 +1,30 @@
+// 1st Iter
+// min: list[0]
+// max: list[30]
+// mid: (list.length/2)
+// currentElem: list[mid] = 'Lisa'
+
+// 2nd Iter 
+// min: list[0]
+// max: list[14]
+// mid: list[7]
+// currentElem: list[mid]='Ian'
+
+// 3rd Iter
+// min: list[0]
+// max: list[6]
+// mid: list[3]
+// currentElem: 
+
+// 4th Iter
+// min: list[4]
+// max: list[6]
+// mid: list[5]
+// currentElem: 
+
+//Searching for Henry in class array
+//can use binary search on numbers, arrays, etc.
+
+var list = ["Bradley", "Daniel", "Darragh", "Eric", "Francis", "Heather", "Humberto", "Ian", "James", "Jamie", "Justin", "Justin", "Katie", "Keala", "Kristian", "Lisa", "Marcel", "Melissa", "Nathan", "Neil", "Nick", "Olivia", "Peter", "Sakina", "Sarad", "Sharon", "Stephen", "Terry", "Tika", "Tom", "William"]
+
+console.log(list[0]);
